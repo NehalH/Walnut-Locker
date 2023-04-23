79363ec5ed243d661bd5d48f235f0a91fd4e6a55
Nehal Hosaliakr <nhosalikar@gmail.com>
2023-04-23 18:36:08 +0530

    Add password dialogue to grant script

--- grant.py
+++ grant.py
@@ -2,6 +2,10 @@ import os
 import getpass
 from cryptography.fernet import Fernet
 from PyQt5.QtWidgets import QApplication, QFileDialog
+from PyQt5.QtWidgets import QDialog, QLabel, QLineEdit, QPushButton, QVBoxLayout
+import sys
+from PyQt5.QtWidgets import QApplication
+
 
 """
 TODO:
@@ -9,52 +13,88 @@ TODO:
 
 """
 
-def file_dialogue():
+def pass_dialogue():
+    app = QApplication(sys.argv)
+    password_dialog = QDialog()
+    password_dialog.setWindowTitle("Enter Password")
+    layout = QVBoxLayout()
 
-	app = QApplication([])
-	file_dialog = QFileDialog()
-	file_dialog.setFileMode(QFileDialog.ExistingFile)
-	if file_dialog.exec_() == QFileDialog.Accepted:
-		file_path = file_dialog.selectedFiles()[0]
-		print(f"Selected file: {file_path}")
-		return file_path
-	else:
-		print("File selection cancelled")
+    label = QLabel("Password:")
+    layout.addWidget(label)
+
+    password_field = QLineEdit()
+    password_field.setEchoMode(QLineEdit.Password)
+    layout.addWidget(password_field)
+
+    ok_button = QPushButton("OK")
+    ok_button.clicked.connect(password_dialog.accept)
+    layout.addWidget(ok_button)
+
+    cancel_button = QPushButton("Cancel")
+    cancel_button.clicked.connect(password_dialog.reject)
+    layout.addWidget(cancel_button)
+
+    password_dialog.setLayout(layout)
+
+    if password_dialog.exec_() == QDialog.Accepted:
+        password = password_field.text()
+        return password
+
+    return None
+
+
+def file_dialogue():
+    app = QApplication([])
+    file_dialog = QFileDialog()
+    file_dialog.setFileMode(QFileDialog.ExistingFile)
+    if file_dialog.exec_() == QFileDialog.Accepted:
+        file_path = file_dialog.selectedFiles()[0]
+        print(f"Selected file: {file_path}")
+        return file_path
+    else:
+        print("File selection cancelled")
+        return None
+
 
 def validate_path(file_path):
+    if not os.path.exists(file_path):
+        print("Error: File path does not exist")
+        exit()
 
-	if not os.path.exists(file_path):
-		print("Error: File path does not exist")
-		exit()
-		
-	if os.access(file_path, os.W_OK):
-		print("Error: File is already open")
-		exit()
+    if os.access(file_path, os.W_OK):
+        print("Error: File is already open")
+        exit()
+
 
 def fetch_pass():
+    with open("password.txt", "rb") as file:
+        stored_password = file.read()
+    return stored_password
 
-	with open("password.txt", "rb") as file:
-		stored_password = file.read()
-	return stored_password
 
 def decrypt(encrypted_password):
+    # key = getpass.getpass("Enter the decryption key: ").encode()
+    key = b'q6IDZLj0VHEgiYtn-5jejIEbMJyUnBi5cDLlOuyC3VU='
+    fernet = Fernet(key)
+    return fernet.decrypt(encrypted_password).decode()
 
-	#key = getpass.getpass("Enter the decryption key: ").encode()
-	key = b'q6IDZLj0VHEgiYtn-5jejIEbMJyUnBi5cDLlOuyC3VU='
-	fernet = Fernet(key)
-	return fernet.decrypt(encrypted_password).decode()
 
 def auth(password, stored_password):
+    if password == stored_password:
+        # Restore permissions for the owner of the file
+        os.chmod(file_path, 0o100700)
+        print("File unlocked successfully!")
+    else:
+        print("Invalid password. File remains locked.")
 
-	if password == stored_password:
-		# Restore permissions for the owner of the file
-		os.chmod(file_path, 0o100700)
-		print("File unlocked successfully!")
-	else:
-		print("Invalid password. File remains locked.")
-    
+
 file_path = file_dialogue()
+if file_path is None:
+    exit()
 validate_path(file_path)
-password = getpass.getpass("Enter the password to unlock the file: ")
+password = pass_dialogue()
+if password is None:
+    exit()
 stored_password = decrypt(fetch_pass())
 auth(password, stored_password)
+
