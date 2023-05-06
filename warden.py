c0d1f317291a7b39f7817da7e285d05675c332f1
Nehal Hosaliakr <nhosalikar@gmail.com>
2023-05-06 20:52:39 +0530

    Add inotify to warden script

--- warden.py
+++ warden.py
@@ -13,19 +13,21 @@ def is_file_locked(file_path):
         locked_files = lock_file.read().splitlines()
         return file_path in locked_files
 
-def on_access_event(event):
-    # check if the accessed file is locked
-    if is_file_locked(os.path.join(event[2], event[3])):
+def on_write_event(event):
+    # check if the written file is locked
+    full_file_path = os.path.abspath(os.path.join(path,filename))
+    print(full_file_path)
+    if is_file_locked(full_file_path):
         # run the Unlock.py script
         os.system(f"python3 {unlock_script_path}")
 
 # create the inotify event listener
 notifier = inotify.adapters.Inotify()
-notifier.add_watch(".")
+notifier.add_watch("./test",mask=inotify.constants.IN_OPEN)
 
-# listen for the ACCESS event
+# listen for the WRITE event
 for event in notifier.event_gen(yield_nones=False):
     (_, type_names, path, filename) = event
-    if "IN_ACCESS" in type_names:
-        print("Haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-        os.system(f"python3 {unlock_script_path}")
+    print('-')
+    if "IN_OPEN" in type_names:
+        on_write_event(event)
