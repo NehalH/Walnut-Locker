e7621ea10b6a65eb0323039daea319267cc36146
Nehal Hosaliakr <nhosalikar@gmail.com>
2023-07-16 19:11:35 +0530

    Add auditd warden script

--- src/warden.py
+++ src/warden.py
@@ -1,35 +1,40 @@
-import inotify.adapters
+import subprocess
+import re
 import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # path to the lock file
-lock_file_path = os.path.join(script_dir, "data", "lockfile.txt")
+lockfiles_path = os.path.join(script_dir, "data", "lockfile.txt")
 
 # path to the Unlock.py script
 unlock_script_path = os.path.join(script_dir, "grant.py")
 
-def is_file_locked(file_path):
-    # check if the file is listed in the lock file
-    with open(lock_file_path, "r") as lock_file:
-        locked_files = lock_file.read().splitlines()
-        return file_path in locked_files
+def run_grant_script(filepath):
+    # Read the lockfiles.txt file to get the list of locked file paths
+    with open(lockfiles_path, 'r') as lockfile:
+        locked_files = lockfile.read().splitlines()
 
-def on_write_event(event):
-    # check if the written file is locked
-    full_file_path = os.path.abspath(os.path.join(path,filename))
-    print(full_file_path)
-    if is_file_locked(full_file_path):
-        # run the Unlock.py script
-        os.system(f"python3 {unlock_script_path}")
+    # Check if the accessed file path is in the locked files list
+    if filepath in locked_files:
+        # Perform actions for granted access (e.g., run the `grant.py` script)
+        subprocess.call(['python3', 'grant.py', unlock_script_path])
 
-# create the inotify event listener
-notifier = inotify.adapters.Inotify()
-notifier.add_watch("./test",mask=inotify.constants.IN_OPEN)
+# Set up audit rules to capture open system calls for file access
+options = "-a exit,always -F arch=b64 -S open"
+auditctl_command = f"auditctl {options}"
 
-# listen for the WRITE event
-for event in notifier.event_gen(yield_nones=False):
-    (_, type_names, path, filename) = event
-    print('-')
-    if "IN_OPEN" in type_names:
-        on_write_event(event)
+# Run the auditctl command to set up the audit rules
+subprocess.call(auditctl_command, shell=True)
+
+# Enter a loop to read auditd output and detect file access events
+with subprocess.Popen(['auditd', '-f'], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as proc:
+    for line in proc.stdout:
+        # Check if the line contains the 'file-access' keyword
+        if 'file-access' in line:
+            # Extract the file path from the line using regular expressions
+            match = re.search(r"obj=(.*?)[\s|,]", line)
+            if match:
+                filepath = match.group(1)
+                # Run the grant script for locked file paths
+                run_grant_script(filepath)
